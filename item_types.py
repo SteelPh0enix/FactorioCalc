0a76abd5810a289adef06c824549be8f6c2a0119
SteelPh0enix <wojciech_olech@hotmail.com>
2024-11-09 11:58:08 +0100

    Item types/items fixups

--- item_types.py
+++ item_types.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 
-@dataclass
+@dataclass(frozen=True)
 class Entity:
     name: str
 
@@ -12,36 +12,36 @@ class Entity:
     def __hash__(self) -> int:
         return hash(self.name)
 
-@dataclass
+@dataclass(frozen=True)
 class Item(Entity):
     stack_size: int
     rocket_capacity: int
 
-@dataclass
+@dataclass(frozen=True)
 class Fuel(Item):
     fuel_value: int
     "Energy possible to acquired from single unit of fuel, in jules"
 
 
-@dataclass
+@dataclass(frozen=True)
 class ItemContainer(Item):
     capacity: int
     """Capacity, in stacks"""
 
 
-@dataclass
+@dataclass(frozen=True)
 class FluidTank(Item):
     volume: float
     """Volume, in fluid units"""
 
 
-@dataclass
+@dataclass(frozen=True)
 class Belt(Item):
     speed: int
     """Belt speed, in moved items per second"""
 
 
-@dataclass
+@dataclass(frozen=True)
 class Inserter(Item):
     rotation_speed: int
     """Rotation speed, in degrees per second"""
@@ -49,14 +49,14 @@ class Inserter(Item):
     """How many items an inserter can pick up at once"""
 
 
-@dataclass
+@dataclass(frozen=True)
 class ElectricPole(Item):
     wire_reach: float
     """How long can a wire reach this pole"""
     supply_area: tuple[int, int]
     """Size of supply area in (width, height) format"""
 
-@dataclass
+@dataclass(frozen=True)
 class Robot(Item):
     capacity: int
     """Cargo capacity of a robot, in items"""
@@ -65,7 +65,7 @@ class Robot(Item):
     max_power_consumption: float
     """Maximum power consumption, in watts"""
 
-@dataclass
+@dataclass(frozen=True)
 class Miner(Item):
     mining_speed: float
     """Mining speed, in items/sec"""
@@ -74,14 +74,14 @@ class Miner(Item):
     resource_drain: float = 1
     """Resource drain multiplier, 100% == 1"""
 
-@dataclass
+@dataclass(frozen=True)
 class Crafter(Item):
     crafting_speed: float = 1
     """Crafting speed multiplier"""
     base_productivity: float = 1
     """Base productivity of a crafter"""
 
-@dataclass
+@dataclass(frozen=True)
 class Module(Item):
     energy_consumption: float = 0
     """Energy consumption multiplier"""
@@ -92,11 +92,11 @@ class Module(Item):
     productivity: float = 0
     """Productivity multiplier"""
 
-@dataclass
+@dataclass(frozen=True)
 class Bacteria(Item):
     spoil_time: int
     """Spoil time, in seconds"""
 
-@dataclass
+@dataclass(frozen=True)
 class Fluid(Entity):
     pass
